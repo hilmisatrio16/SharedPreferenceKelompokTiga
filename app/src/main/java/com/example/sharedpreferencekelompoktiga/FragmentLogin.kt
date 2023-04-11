9e8fce550dd8fbed795b603abf8904826f89b570
muhammadhilmisatrio <hilmisatrio16@gmail.com>
2023-04-11 23:28:06 +0700

    initial commit

--- app/src/main/java/com/example/sharedpreferencekelompoktiga/FragmentLogin.kt
+++ app/src/main/java/com/example/sharedpreferencekelompoktiga/FragmentLogin.kt
@@ -1,59 +1,57 @@
 package com.example.sharedpreferencekelompoktiga
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.sharedpreferencekelompoktiga.databinding.FragmentLoginBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentLogin.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentLogin : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var sharedPref : SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        binding.clickListener = this
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentLogin.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentLogin().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sharedPref = requireContext().getSharedPreferences("dataprefs", Context.MODE_PRIVATE)
+
+
     }
+
+    fun checkAccount(){
+
+        var inputUsername = binding.inputUsername.text.toString()
+        var inputPassword = binding.inputPassword.text.toString()
+        if(inputUsername.isNotEmpty() && inputPassword.isNotEmpty()){
+            val getUsername = sharedPref.getString("username", "")
+            val getPassword = sharedPref.getString("password", "")
+
+            if(inputUsername == getUsername && inputPassword == getPassword){
+                findNavController().navigate(R.id.action_fragmentLogin_to_fragmentHome)
+                Toast.makeText(context, "$getUsername Login berhasil", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(context, "Username atau password yang anda masukan salah!", Toast.LENGTH_SHORT).show()
+            }
+
+        }else{
+            Toast.makeText(context, "Username atau Password tidak boleh kosong!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
