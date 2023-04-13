851da22bfc25d15997d83af45790354a7d37b4f8
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-04-13 23:23:06 +0700

    adding splash and adding Home

--- app/src/main/java/com/example/sharedpreferencekelompoktiga/FragmentHome.kt
+++ app/src/main/java/com/example/sharedpreferencekelompoktiga/FragmentHome.kt
@@ -1,59 +1,50 @@
 package com.example.sharedpreferencekelompoktiga
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.sharedpreferencekelompoktiga.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentHome.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentHome : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var getPref:SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        binding.clickListeners = this
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentHome.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentHome().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getPref = requireContext().getSharedPreferences("dataprefs", Context.MODE_PRIVATE)
+        binding.username = "Hello," + " " + getPref.getString("username", "null")
+
+        binding.btnLogout.setOnClickListener {
+            logOut()
+        }
     }
+
+
+
+    @SuppressLint("CommitPrefEdits")
+    fun logOut(){
+        val prefs = getPref.edit()
+        prefs.clear()
+        prefs.apply()
+        findNavController().navigate(R.id.action_fragmentHome_to_fragmentLogin)
+    }
+
+
 }
